eb8f98cac6dd6688bd0044b0e20f3ddb9da0bc94
SPRNGLS <contact@springles.xyz>
2026-03-05 21:26:49 +0100

    Delete src directory

--- src/routes/register.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerUser } from "../controllers/registerController.ts";
-
-const router = express.Router();
-
-router.post('/', registerUser);
-
-export default router;
